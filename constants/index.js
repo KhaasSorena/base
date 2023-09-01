57217659d18a000cd772ae689dc922744470e721
Surena <59043626+KhaasSorena@users.noreply.github.com>
2023-09-01 00:07:01 -0700

    Update index.js
    Update Contract

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x04868db3d82ba3b5e37502ede7192a27a103ad5f";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
